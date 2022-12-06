c0f3b2d122f88798029b5b01bd746bbf9b7df687
99985 <119901555+99985@users.noreply.github.com>
2022-12-06 17:57:45 -0500

    More styleing

--- bookmarkletv2.js
+++ bookmarkletv2.js
@@ -5,9 +5,13 @@ let div = document.createElement("div");
 let myHeader = document.createElement('h1')
 let myHeaderReal = document.createElement('center')
 let myCenter = document.createElement('center')
-/*Color!*/
-myCenter.style.color ="5c100d"
-document.body.style.backgroundColor = "5c100d";
+/*styleing :)*/
+myHeader.style.fontFamily = "Impact,Charcoal,sans-serif";
+myHeader.style.color = "#5c100d"
+myCenter.style.fontFamily = "Impact,Charcoal,sans-serif";
+document.getElementById("demo").style.fontSize = "large";
+myCenter.style.color = "#5c100d"
+document.body.style.backgroundColor = "#132a32";
 /*giving the elements text*/
 myHeaderReal.innerText = ("A intresting header")
 myCenter.innerText = ("A intresting center")
