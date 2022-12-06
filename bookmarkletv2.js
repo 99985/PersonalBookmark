0e8e659e53efd1d40a896e08a70f9e0beb4c5028
99985 <119901555+99985@users.noreply.github.com>
2022-12-06 17:40:08 -0500

    color

--- bookmarkletv2.js
+++ bookmarkletv2.js
@@ -5,7 +5,9 @@ let div = document.createElement("div");
 let myHeader = document.createElement('h1')
 let myHeaderReal = document.createElement('center')
 let myCenter = document.createElement('center')
-myCenter.style.color ="#5c100d"
+/*Color!*/
+myCenter.style.color ="5c100d"
+document.body.style.backgroundColor = "5c100d";
 /*giving the elements text*/
 myHeaderReal.innerText = ("A intresting header")
 myCenter.innerText = ("A intresting center")
