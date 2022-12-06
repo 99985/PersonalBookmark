b5b0e7309004ea22812c896323f9df68d669bde1
99985 <119901555+99985@users.noreply.github.com>
2022-12-06 17:37:17 -0500

    Create bookmarkletv2.js

---
+++ bookmarkletv2.js
@@ -0,0 +1,16 @@
+/*clearing everything else but the html*/
+document.write("1");
+/*specifying what i want for each text*/
+let div = document.createElement("div");
+let myHeader = document.createElement('h1')
+let myHeaderReal = document.createElement('center')
+let myCenter = document.createElement('center')
+myCenter.style.color ="#5c100d"
+/*giving the elements text*/
+myHeaderReal.innerText = ("A intresting header")
+myCenter.innerText = ("A intresting center")
+/*adding the elements to the page*/
+myHeader.appendChild(myHeaderReal)
+document.body.appendChild(div)
+document.body.appendChild(myHeader)
+document.body.appendChild(myCenter)
