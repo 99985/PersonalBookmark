c814f089332688f16d7dba98f97e2ecfcdcaebef
99985 <119901555+99985@users.noreply.github.com>
2022-12-07 12:39:07 -0500

    First button + image middle

--- bookmarkletv2.js
+++ bookmarkletv2.js
@@ -1,5 +1,6 @@
 /*clearing everything else but the html*/
 document.write("1");
+
 /*specifying what i want for each text*/
 let div = document.createElement("div");
 let myHeader = document.createElement('h1')
@@ -7,24 +8,44 @@ let myHeaderReal = document.createElement('center')
 let myCenter = document.createElement('center')
 let ButtonPicture = document.createElement('img')
 let img = document.createElement("img");
-img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTiuhxGKEjVXB1iT6j2zms0iPUpZsgaDC6yA&usqp=CAU";
+img.src = "https://picsum.photos/200/301";
+
+/*button code*/
+let can = document.createElement("button");
+can.innerHTML = "Canvas"
+can.addEventListener("click", function () {
+    alert("Opening Canvas in a new tab");
+    window.open("https://dekalb.instructure.com/");
+  });
+
 
 /*styleing :)*/
 myHeader.style.fontFamily = "Impact,Charcoal,sans-serif";
-myHeader.style.color = "#5c100d"
+myHeader.style.color = "#ffee32";
+myHeaderReal.fontSize = "x-large";
+img.style.position = "relative";
 myCenter.style.fontFamily = "Impact,Charcoal,sans-serif";
 myCenter.style.fontSize = "large";
-myCenter.style.color = "#5c100d"
-document.body.style.backgroundColor = "#132a32";
+myCenter.style.color = "#ffee32";
+
+img.style.left = '270px'
+
+can.style.width = '100px'
+can.style.height = '100px'
+can.style.left = '20px'
+can.style.bottom = '20px'
+
+document.body.style.zoom = '200%'
+document.body.style.backgroundColor = "#202020";
 
 /*giving the elements their stuff*/
 myHeaderReal.innerText = ("Welcome back!")
 myCenter.innerText = ("What website do you want to visit today?")
-img.src = "https://picsum.photos/200/301";
 
 /*adding the elements to the page*/
 myHeader.appendChild(myHeaderReal)
-document.body.appendChild(div)
 document.body.appendChild(myHeader)
 document.body.appendChild(myCenter)
+document.body.appendChild(div)
 document.body.appendChild(img);
+document.body.appendChild(can);
